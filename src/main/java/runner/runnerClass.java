c3b2cdc7341866dd2a6538792f904ec5e2906d2b
Raphael-Kariuki <m0ckinjay@yandex.com>
2024-01-11 05:07:00 +0300

    supplier interface

--- src/main/java/runner/runnerClass.java
+++ src/main/java/runner/runnerClass.java
@@ -24,59 +24,66 @@ public class runnerClass {
     public static void main(String[] args) {
      
 
-    //inconsistency while processing stream elements in parallel
+//    //inconsistency while processing stream elements in parallel
+//
+//
+//        StreamProcessor streamProcessor = new StreamProcessor();
+//
+//        for (int i = 1; i <= 10; i++) {
+//            streamProcessor.addNumber(i);
+//        }
+//
+//        // Creating two threads to process stream elements concurrently
+//        Thread thread1 = new Thread(streamProcessor::processNumbers);
+//        Thread thread2 = new Thread(streamProcessor::processNumbers);
+//
+//        thread1.start();
+//        thread2.start();
 
-
-        StreamProcessor streamProcessor = new StreamProcessor();
-
-        for (int i = 1; i <= 10; i++) {
-            streamProcessor.addNumber(i);
-        }
-
-        // Creating two threads to process stream elements concurrently
-        Thread thread1 = new Thread(streamProcessor::processNumbers);
-        Thread thread2 = new Thread(streamProcessor::processNumbers);
-
-        thread1.start();
-        thread2.start();
-
-
+        //supplier interface - defines a single method get - implementation receives no argument 
+        //but produces a result
+        //Can be used to delay the computation of a value until when it's needed, result obtained by calling .get()
+        int var1 = 34, var2 = 23;
+        Supplier<Integer> summation = () -> Integer.sum(var1, var2);
+        System.out.println("" + summation.get());
+        Supplier<Double> x = () -> Math.random();
+        System.out.println("" + x.get());
     }
 }
- class StreamProcessor {
-
-     //new arrayList creation
-        private List<Integer> numbers = new ArrayList<>();
-
-        //method that receives single int as argument and adds it to the arrayList
-        public void addNumber(int number) {
-            numbers.add(number);
-        }
-
-        //method that converts the arrayList to a stream - that processes elements in parallel and (
-        //1. sleeps for 100s, 
-        //2. double the provided int arg and assigns value to variable doubleValue, 
-        //3.prints (associated thread,int arg supplied and double it's value))
-        public void processNumbers() {
-            numbers.parallelStream().forEach(this::processNumber);
-        }
-
-        //1. method that sleeps for 100 secs, takes the int argument supplied and doubles it then assigns it to the doubleValue
-        //2. Prints the thread that processed the number, the number supplied and it's double value
-        //3. removes the supplied number from the arrayList
-        private void processNumber(int number) {
-            // Simulating a time-consuming task
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                System.out.println("" + e.getMessage());
-            }
-
-            // Inconsistent operation - modifying shared data without synchronization
-            int doubledValue = number * 2;
-            System.out.println(Thread.currentThread().getName() + ": Processed " + number + ", Result: " + doubledValue);
-
-            // Modifying shared data without synchronization
-            numbers.remove(Integer.valueOf(number));
-        }
-    }
+// class StreamProcessor {
+//
+//     //new arrayList creation
+//        private List<Integer> numbers = new ArrayList<>();
+//
+//        //method that receives single int as argument and adds it to the arrayList
+//        public void addNumber(int number) {
+//            numbers.add(number);
+//        }
+//
+//        //method that converts the arrayList to a stream - that processes elements in parallel and (
+//        //1. sleeps for 100s, 
+//        //2. double the provided int arg and assigns value to variable doubleValue, 
+//        //3.prints (associated thread,int arg supplied and double it's value))
+//        public void processNumbers() {
+//            numbers.parallelStream().forEach(this::processNumber);
+//        }
+//
+//        //1. method that sleeps for 100 secs, takes the int argument supplied and doubles it then assigns it to the doubleValue
+//        //2. Prints the thread that processed the number, the number supplied and it's double value
+//        //3. removes the supplied number from the arrayList
+//        private void processNumber(int number) {
+//            // Simulating a time-consuming task
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                System.out.println("" + e.getMessage());
+//            }
+//
+//            // Inconsistent operation - modifying shared data without synchronization
+//            int doubledValue = number * 2;
+//            System.out.println(Thread.currentThread().getName() + ": Processed " + number + ", Result: " + doubledValue);
+//
+//            // Modifying shared data without synchronization
+//            numbers.remove(Integer.valueOf(number));
+//        }
+//    }
